d2d63325e89971ad99835986c0eb3aab62e081f1
Peter O'Shaughnessy <peter.oshaughnessy@pebblecode.com>
2015-12-03 14:39:16 +0000

    Add plugins

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -3,14 +3,31 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-ble-central/www/ble.js",
         "id": "cordova-plugin-ble-central.ble",
-        "pluginId": "cordova-plugin-ble-central",
         "clobbers": [
             "ble"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "id": "cordova-plugin-console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "id": "cordova-plugin-console.logger",
+        "clobbers": [
+            "cordova.logger"
         ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-ble-central": "1.0.1",
+    "cordova-plugin-console": "1.0.2"
+}
 // BOTTOM OF METADATA
 });
